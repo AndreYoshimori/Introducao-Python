3d06e3c932ce27783f6591326bea4cafe5bb5d0a
Andre Yoshimori <andreyoshimori@gmail.com>
2026-02-02 23:53:08 -0300

    Começando o capítulo 10

---
+++ capitulo10/ex_01.py
@@ -0,0 +1,3 @@
+# Exercício 10.1: Adicione os atributos tamanho e marca à classe Televisão.
+# Crie dois objetos Televisão e atribua tamanhos e marcas diferentes.
+# Depois, imprima o valor desses atributos de forma a confirmar a independência dos valores de cada instância (objeto).
